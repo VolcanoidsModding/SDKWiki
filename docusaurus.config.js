595995795fa349bcbe7b8c7975c38a08cbacfdc2
MelodicAlbuild <ralexdrum@gmail.com>
2021-04-07 08:52:09 -0500

    Icon Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "img/ModdingCOG.ico",
   organizationName: "VolcanoidsModding", // Usually your GitHub org/user name.
   projectName: "SDKWiki", // Usually your repo name.
   themeConfig: {
